b9c496f60afb6d037ad7e38ad651a0511c31f47f
Oleh Hryhorov <ohryhorov@mirantis.com>
2018-02-08 13:02:53 +0200

    Promoting the list salt-formula packages
    The patch promotes nightly/testing salt-formula had been defined explicitely
    in order to avoid disappearing packages.
    
    Change-Id: Id8d66d066cad302c4f003774d7fe94eeb17d18a8

--- oscc-ci-pipeline.groovy
+++ oscc-ci-pipeline.groovy
@@ -266,7 +266,7 @@ node('python'){
        if (common.validInputParam('AUTO_PROMOTE') && AUTO_PROMOTE.toBoolean() == true) {
             for (prefix in prefixes) {
                 common.successMsg("${components} repo with prefix: ${prefix} distribution: ${distribution} snapshot: ${snapshot} will be promoted to testing")
-                aptly.promotePublish(server['url'], "${prefix}/${distribution}", 'xenial/testing', 'false', components, '', '', '-d --timeout 1200', '', '')
+                aptly.promotePublish(server['url'], "${prefix}/${distribution}", 'xenial/testing', 'false', components, OPENSTACK_COMPONENTS_LIST, '', '-d --timeout 1200', '', '')
             }
        }
    }
