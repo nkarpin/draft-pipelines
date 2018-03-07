b7103839b4bfcaf11ed88e8e723c22d65a47c675
Vasyl Saienko <vsaienko@mirantis.com>
2018-03-07 13:09:17 +0200

    Add deploy-virtual-model-change-pipeline job.
    Related-Prod: PROD-18087
    
    Change-Id: Ie3fe16efb0264d1b3ba5a4319c24f58bcb7e2405

---
+++ deploy-virtual-model-change-pipeline.groovy
@@ -0,0 +1,116 @@
+/**
+*
+* Test specified virtual model changes.
+*
+* Expected parameters:
+*    CREDENTIALS_ID               ID of jenkins credentials to be used when connecting to gerrit.
+
+*    STACK_RECLASS_ADDRESS        Git URL to reclass model to use for deployment.
+*    STACK_RECLASS_BRANCH         Git branch or ref of cluster model to test.
+*
+*    GERRIT_*                     Gerrit trigger plugin variables.
+*    TEST_CLUSTER_NAMES           Comma separated list of cluster names to test.
+*
+*    There are 2 options to run the pipeline:
+*    1. Manually.
+*       In this case need to define SOURCES parameter. See above.
+*    2. Automatically by Gerrit trigger.
+*       In this case Gerrit trigger adds GERRIT_* paramters to the build and the pipeline will use it.
+*       SOURCES parameter should be empty.
+*
+*/
+
+def common = new com.mirantis.mk.Common()
+def aptly = new com.mirantis.mk.Aptly()
+def http = new com.mirantis.mk.Http()
+def gerrit = new com.mirantis.mk.Gerrit()
+
+defcheckouted = false
+
+def testClusterNames
+def messages = ["${env.BUILD_URL}"]
+
+def gerritRef
+try {
+  gerritRef = GERRIT_REFSPEC
+} catch (MissingPropertyException e) {
+  gerritRef = STACK_RECLASS_BRANCH
+}
+
+def gerritUrl
+def gitUrl
+try {
+  gerritUrl = "${GERRIT_SCHEME}://${GERRIT_NAME}@${GERRIT_HOST}:${GERRIT_PORT}/${GERRIT_PROJECT}"
+  gerritUrlAnonymous = "https://${GERRIT_HOST}/${GERRIT_PROJECT}"
+} catch (MissingPropertyException e) {
+  gerritUrl = STACK_RECLASS_ADDRESS
+  gerritUrlAnonymous = STACK_RECLASS_ADDRESS
+}
+
+def gerritBranch
+try {
+  gerritBranch = GERRIT_BRANCH
+} catch (MissingPropertyException e) {
+  gerritBranch = STACK_RECLASS_BRANCH
+}
+
+def systestJobPrefix = 'oscore-formula-systest-virtual_mcp11_aio-'
+if (common.validInputParam('SYSTEST_JOB_PREFIX')) {
+    systestJobPrefix = SYSTEST_JOB_PREFIX
+}
+
+def setGerritBuildString(buildObj, cluster_name){
+    return "* ${cluster_name} ${buildObj.absoluteUrl} : ${buildObj.result} ${buildObj.durationString}"
+}
+
+node("python") {
+  stage("checkout") {
+    if(gerritRef != '' && gerritUrl != '') {
+        checkouted = gerrit.gerritPatchsetCheckout(gerritUrl, gerritRef, "HEAD", CREDENTIALS_ID)
+    } else {
+      throw new Exception("Cannot checkout gerrit: ${gerritUrl} branch: ${gerritRef} failed")
+    }
+  }
+
+  if(common.validInputParam('TEST_CLUSTER_NAMES')){
+    common.infoMsg("TEST_CLUSTER_NAMES was set explicitly using its value: ${TEST_CLUSTER_NAMES}")
+    testClusterNames = TEST_CLUSTER_NAMES.tokenize('/')
+  } else {
+    // TODO: add logic to automatically pick changed mode
+  }
+
+  def testBuilds = [:]
+  stage('Deploy clusters') {
+    def deploy_release = [:]
+    def release = gerritBranch.tokenize('/').last()
+    for (cluster_name in testClusterNames) {
+      def cn = cluster_name
+      deploy_release["Deploy ${cluster_name}"] = {
+        node('oscore-testing') {
+          testBuilds["${cluster_name}"] = build job: "${systestJobPrefix}${release}", propagate: false, parameters: [
+            [$class: 'StringParameterValue', name: 'STACK_RECLASS_ADDRESS', value: "${gerritUrlAnonymous}"],
+            [$class: 'StringParameterValue', name: 'STACK_RECLASS_BRANCH', value: "${gerritRef}"],
+            [$class: 'StringParameterValue', name: 'STACK_CLUSTER_NAME', value: "${cn}"],
+            [$class: 'StringParameterValue', name: 'FORMULA_PKG_REVISION', value: "nightly"],
+            ]
+          }
+        }
+      }
+      parallel deploy_release
+    }
+
+    def test_result = true
+    stage('Managing deployment results') {
+      testBuilds.each {
+        if (it.value != 'SUCCESS') {
+          test_result = false
+        }
+        messages.add(0, setGerritBuildString(it.value, it.key))
+        setGerritReview customUrl: messages.join('\n')
+      }
+    }
+
+  if (!test_result) {
+    error('Some of deploy jobs failed')
+  }
+}
